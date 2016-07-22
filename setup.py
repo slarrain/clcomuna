a0c8a71d87aebfab33c2f4e51b5948732b365d31
Santiago Larrain <slarrain@uchicago.edu>
2016-07-21 20:25:18 -0400

    First commit It is working but its using a hardcoded version of a comunas dictionary I will change it to use a csv thats easier to change. So this is a prior commit.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='clcomuna',
+      version='0.1',
+      description='Given the name of a comuna in Chile, it returns its code',
+      url='http://github.com/slarrain/clcomuna',
+      author='Santiago Larrain',
+      author_email='santiagolarrain@gmail.com',
+      license='MIT',
+      packages=['clcomuna'],
+      zip_safe=False)
