bac0db47ee80fb709cbccd9758d8b8db49b002e6
Santiago Larrain <slarrain@uchicago.edu>
2017-07-03 22:15:57 -0400

    Add 2 more comunas to the csv

--- setup.py
+++ setup.py
@@ -5,8 +5,9 @@ def readme():
         return f.read()
 
 setup(name='clcomuna',
-      version='1.1',
+      version='1.1.1',
       description='Given the name of a comuna in Chile, it returns its code',
+      long_description="README.rst"
       url='http://github.com/slarrain/clcomuna',
       author='Santiago Larrain',
       author_email='santiagolarrain@gmail.com',
